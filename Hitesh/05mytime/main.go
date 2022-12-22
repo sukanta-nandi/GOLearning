95cdcddecd9eb8d2c5425abd8476ab2cd893f947
sukanta-nandi <sukantanandi0@gmail.com>
2022-12-22 16:26:33 +0530

    use of time

---
+++ Hitesh/05mytime/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to Time Golang")
+
+	presentTime := time.Now()
+	fmt.Println("Current Time: ", presentTime)
+
+	// formatting
+	fmt.Println("Current Time: ", presentTime.Format("01-02-2006 15:04:05 Monday")) // this is the standard time for formatting
+
+	createdDate := time.Date(2020, time.August, 12, 23, 23, 0, 0, time.UTC)
+	fmt.Println("created date: ", createdDate.Format("01-02-2006 Monday"))
+}
