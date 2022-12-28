8acff59dc181532710cce58408c9415a7c191f06
sukanta-nandi <sukantanandi0@gmail.com>
2022-12-28 15:32:13 +0530

    handling json

---
+++ LCO/22json/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	Name     string `json:"coursename"` //aliases for key
+	Price    int
+	Platform string
+	password string   `json:"-"` //do not show field
+	Tags     []string `json:"tags,omitempty"`
+}
+
+func main() {
+	fmt.Println("Handling JSON in Golang")
+	EncodeJson()
+}
+
+func EncodeJson() {
+	lcoCourses := []course{
+		{"React JS Bootcamp", 299, "LearnCodeOnline.in", "abc123", []string{"web-dev", "js"}},
+		{"MERN Bootcamp", 199, "LearnCodeOnline.in", "bcd123", []string{"fullstack", "js"}},
+		{"Angular Bootcamp", 299, "LearnCodeOnline.in", "efg123", nil},
+	}
+
+	// package this data as JSON
+	// finalJson, err := json.Marshal(lcoCourses)
+	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", finalJson)
+}
